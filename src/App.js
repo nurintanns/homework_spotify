b97177dfbc333f2130839107bb597b769c279135
Nur Intan Sari <71863740+nurintanns@users.noreply.github.com>
2022-03-28 16:59:43 +0700

    homework modul 2 session 3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import data from './music_data/music_data.js';
-import {AlbumImage, AlbumName, ButtonUrl, AlbumDesc} from './components/newcomponents/index.js';
+import ObjectLoop from './components/newcomponents/track.js'
 
 function App() {
   return (
     <div className="App">
-      <AlbumImage src={data.album.images[0].url}>
-        <AlbumDesc name={data.name}/>
-      </AlbumImage>
-      <AlbumName name={data.album.name} artist={data.artists[0].name}>
-        < ButtonUrl url={data.album.artists[0].external_urls.spotify} />
-      </ AlbumName>
+     <ObjectLoop/>
     </div>
   );
 }
