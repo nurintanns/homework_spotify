9be1b1fb0108fc106491bd056ec70d901b773655
Nur Intan Sari <71863740+nurintanns@users.noreply.github.com>
2022-04-06 23:17:36 +0700

    homework modul 4 session 1

---
+++ src/Redux/userToken.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    token: "",
+}
+
+export const userTokenSlice = createSlice({
+    name: 'userToken',
+    initialState,
+    reducers: {
+        saveToken: (state, action) => {
+        state.token += action.payload
+        },
+    },
+})
+
+export const { saveToken } = userTokenSlice.actions
+
+export default userTokenSlice.reducer
